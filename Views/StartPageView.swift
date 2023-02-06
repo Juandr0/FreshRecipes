e6999ffe0dd6f66c07d4677ee4e8cacc6a01074a
Alexander Carlsson <officialskytten@gmail.com>
2023-02-06 11:28:06 +0100

    added a navigationbar to the bottom of the app

--- Views/StartPageView.swift
+++ Views/StartPageView.swift
@@ -13,57 +13,78 @@ struct StartPageView: View {
     
     var body: some View {
         
-        NavigationView {
-            
-            VStack {
-                Text("Fresh Recepies")
-                    .font(.title3)
-                Spacer()
-                NavigationLink(destination: RecepieView()) {
-                    HStack{
-                        Text("Recept")
-                        Spacer()
-                        Image(systemName: "fork.knife")
-                    }
+        TabView {
+                RecepieView()
+                .tabItem {
+                    Image(systemName: "fork.knife")
+                    Text("Recepies")
                 }
-                
-                NavigationLink(destination: ShoppingListView()) {
-                    HStack{
-                        HStack {
-                            Text("Inköpslista")
-                            Spacer()
-                            Image(systemName: "list.clipboard")
-                        }
-                    }
-                } 
-               Spacer()
-                
+    
+            VStack {
+                ShoppingListView()
             }
-            .padding(15)
-            .buttonStyle(.borderedProminent)
-            .controlSize(.large)
-            .font(.title2)
-            .onAppear{
-
-
-
-//                db.collection("recepies").document().setData([
-//                    "name" : nyttRecept.name,
-//                    "portions" : nyttRecept.portions,
-//                    "ingredients" : nyttRecept.ingredients,
-//                    "allergenics" : nyttRecept.allergenics,
-//                    "instructions" : nyttRecept.instructions,
-//                    "cookingtimeMinutes" : nyttRecept.cookingtimeMinutes
-//                ])
+                    .tabItem{
+                        Image(systemName: "list.clipboard")
+                        Text("Shopping list")
+                  
             }
+           
         }
-        .navigationTitle("Text")
+        
+        
+        
+        //        NavigationView {
+        //
+        //            VStack {
+        //                Text("Fresh Recepies")
+        //                    .font(.title3)
+        //                Spacer()
+        //                NavigationLink(destination: RecepieView()) {
+        //                    HStack{
+        //                        Text("Recept")
+        //                        Spacer()
+        //                        Image(systemName: "fork.knife")
+        //                    }
+        //                }
+        //
+        //                NavigationLink(destination: ShoppingListView()) {
+        //                    HStack{
+        //                        HStack {
+        //                            Text("Inköpslista")
+        //                            Spacer()
+        //                            Image(systemName: "list.clipboard")
+        //                        }
+        //                    }
+        //                }
+        //               Spacer()
+        //
+        //            }
+        //            .padding(15)
+        //            .buttonStyle(.borderedProminent)
+        //            .controlSize(.large)
+        //            .font(.title2)
+        //            .onAppear{
+        //
+        //
+        //
+        ////                db.collection("recepies").document().setData([
+        ////                    "name" : nyttRecept.name,
+        ////                    "portions" : nyttRecept.portions,
+        ////                    "ingredients" : nyttRecept.ingredients,
+        ////                    "allergenics" : nyttRecept.allergenics,
+        ////                    "instructions" : nyttRecept.instructions,
+        ////                    "cookingtimeMinutes" : nyttRecept.cookingtimeMinutes
+        ////                ])
+        //            }
+        //        }
+        //        .navigationTitle("Text")
+        //    }
     }
+    
+    
+    //struct StartPageView_Previews: PreviewProvider {
+    //    static var previews: some View {
+    //        StartPageView()
+    //    }
+    //}
 }
-
-
-//struct StartPageView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        StartPageView()
-//    }
-//}
