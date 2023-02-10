5194cd505d17466854a001d702142f67dc25e282
Alexander Carlsson <officialskytten@gmail.com>
2023-02-10 12:48:27 +0100

    added an add-button that takes user to navigation view

--- Views/ShoppingListView.swift
+++ Views/ShoppingListView.swift
@@ -23,9 +23,10 @@ struct ShoppingListView: View {
         //Alternativt hitta ngn funktion som tar in en int som begränsar och sätt den till list.size
         
         NavigationView {
-            VStack {
-                List {
-                    ForEach(recepies.userItems){item in
+            ZStack{
+                VStack {
+                    List {
+                        ForEach(recepies.userItems){item in
                             if !item.isBought{
                                 HStack{
                                     Button(item.itemName, action: ({
@@ -34,81 +35,93 @@ struct ShoppingListView: View {
                                             docRef.document(item.id!).updateData([
                                                 "isBought" : !item.isBought
                                             ])
-                                        }  
+                                        }
                                     }))
                                     Spacer()
                                     Image(systemName: "square")
                                         .backgroundStyle(.white)
                                 }
                             }
-                    }.onDelete() {indexSet in
-                        for index in indexSet{
-                            guard let user = Auth.auth().currentUser else {return}
-                            let item = recepies.userItems[index]
-                            if let id = item.id {
-                                db.collection("users").document(user.uid)
-                                    .collection("userItems").document(id).delete()
+                        }.onDelete() {indexSet in
+                            for index in indexSet{
+                                guard let user = Auth.auth().currentUser else {return}
+                                let item = recepies.userItems[index]
+                                if let id = item.id {
+                                    db.collection("users").document(user.uid)
+                                        .collection("userItems").document(id).delete()
+                                }
                             }
-                        }
-                        if (recepies.userItems.isEmpty){
-                            for recepieID in recepies.addedRecepieID {
-                                db.collection("users").document(currentUser!.uid).collection("addedRecepieID").document(recepieID).delete()
+                            if (recepies.userItems.isEmpty){
+                                for recepieID in recepies.addedRecepieID {
+                                    db.collection("users").document(currentUser!.uid).collection("addedRecepieID").document(recepieID).delete()
+                                }
                             }
+                            
                         }
                         
-                    }
-                    
-                    ForEach(recepies.userItems){ item in
-                        if item.isBought{
-                            HStack{
-                                Button(item.itemName, action: ({
-                                    if let currentUser {
-                                        let docRef = db.collection("users").document(currentUser.uid).collection("userItems")
-                                        docRef.document(item.id!).updateData([
-                                            "isBought" : !item.isBought
-                                        ])
-                                    }
+                        ForEach(recepies.userItems){ item in
+                            if item.isBought{
+                                HStack{
+                                    Button(item.itemName, action: ({
+                                        if let currentUser {
+                                            let docRef = db.collection("users").document(currentUser.uid).collection("userItems")
+                                            docRef.document(item.id!).updateData([
+                                                "isBought" : !item.isBought
+                                            ])
+                                        }
+                                        
+                                    }))
+                                    .strikethrough()
+                                    Spacer()
+                                    Image(systemName: "checkmark.square")
+                                        .backgroundStyle(.white)
                                     
-                                }))
-                                .strikethrough()
-                                Spacer()
-                                Image(systemName: "checkmark.square")
-                                    .backgroundStyle(.white)
-                                  
+                                }
                             }
                         }
-                    }
-                    .onDelete() {indexSet in
-                        guard let user = Auth.auth().currentUser else {return}
-                        for index in indexSet{
-                            let item = recepies.userItems[index]
-                            if let id = item.id {
-                                db.collection("users").document(user.uid)
-                                    .collection("userItems").document(id).delete()
+                        .onDelete() {indexSet in
+                            guard let user = Auth.auth().currentUser else {return}
+                            for index in indexSet{
+                                let item = recepies.userItems[index]
+                                if let id = item.id {
+                                    db.collection("users").document(user.uid)
+                                        .collection("userItems").document(id).delete()
+                                }
+                            }
+                            if (recepies.userItems.isEmpty){
+                                for recepieID in recepies.addedRecepieID {
+                                    db.collection("users").document(currentUser!.uid).collection("addedRecepieID").document(recepieID).delete()
+                                }
                             }
                         }
-                        if (recepies.userItems.isEmpty){
-                            for recepieID in recepies.addedRecepieID {
-                                db.collection("users").document(currentUser!.uid).collection("addedRecepieID").document(recepieID).delete()
-                            }
-                        }
-                    }
-                    .foregroundColor(.green)
-                       
-                  
-                    
-                }.listStyle(.inset)
-                    .navigationBarTitle("Inköpslista")
-                    .navigationBarItems(trailing: NavigationLink(destination: AddRecepieItemsManuallyView()) {
-                        HStack {
-                            Text("Lägg till")
-                            Image(systemName: "plus.circle")
-                        }
-                       
-                    })
+                        .foregroundColor(.green)
+                        
+                        
+                        
+                    }//List end
                 
-                   
-            }
+                    .listStyle(.inset)
+                        .navigationBarTitle("Inköpslista")
+                        .navigationBarItems(trailing: NavigationLink(destination: AddRecepieItemsManuallyView()) {
+                            HStack {
+                                Text("Lägg till")
+                                Image(systemName: "plus.circle")
+                            }
+                            
+                        })
+          
+                        NavigationLink(destination: AddRecepieItemsManuallyView()){
+                            HStack{
+                                Spacer()
+                                Image(systemName: "plus.circle")
+                                    .resizable()
+                                    .frame(width: 50, height: 50)
+                                    .foregroundColor(.green)
+                                    .padding(EdgeInsets(top: 0, leading: 0, bottom: 30, trailing: 30))
+                            }
+                        }     
+                    } //VStack end
+                } //ZStack end
             }
 
         }
